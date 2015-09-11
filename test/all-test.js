24bae0adc8a0a47693d46f57ac2cb3ade0d4f9a4
Mark Feltner <mfeltner@widen.com>
2015-09-11 01:19:43 -0500

    test: better tests

--- test/all-test.js
+++ test/all-test.js
@@ -6,3 +6,47 @@ test('sanity check', function(t){
     t.plan(1);
     t.ok(aws);
 });
+
+test('s3', function(t){
+    t.plan(1);
+
+    aws.respond({
+        brain: {},
+        command: 'aws',
+        log: {
+            info: console.log,
+            error: console.error
+        },
+        args: 's3 listBuckets',
+        incoming_message: {
+            user_name: 'feltnerm'
+        }
+    }).then(function(result){
+        t.pass(result);
+    }).catch(function(error){
+        t.fail(error);
+    });
+
+});
+
+test('ec2', function(t){
+    t.plan(1);
+
+    aws.respond({
+        brain: {},
+        command: 'aws',
+        log: {
+            info: console.log,
+            error: console.error
+        },
+        args: 'ec2 desc',
+        incoming_message: {
+            user_name: 'feltnerm'
+        }
+    }).then(function(result){
+        t.pass(result);
+    }).catch(function(error){
+        t.fail(error);
+    });
+
+})
